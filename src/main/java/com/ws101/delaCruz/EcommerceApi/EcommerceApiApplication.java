db47cad381642ce51e5f1f2fe17c056733011cc5
hpedradoja-stack <hpedradoja@gmail.com>
2026-04-24 23:53:04 +0800

    Create EcommerceApiApplication.java
    task 1.2

---
+++ src/main/java/com/ws101/delaCruz/EcommerceApi/EcommerceApiApplication.java
@@ -0,0 +1,12 @@
+package com.ws101.delaCruz.EcommerceApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceApiApplication.class, args);
+    }
+}
